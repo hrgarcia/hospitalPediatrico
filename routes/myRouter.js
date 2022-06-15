9a3272e23fca42668213c816016c14619bd4d677
ShinShanPoo <vgalanmartinez@escuelasproa.edu.ar>
2022-06-15 15:07:27 -0300

    seccionAdmin

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -9,5 +9,8 @@ router.route("/").get(myController.inicio);
 router.route("/login").get(adminController.vista).post(adminController.logine);
 router.route("/logout").get(adminController.logout);
 router.route("/subirPost").get(adminController.postear2);
+router.route("/seccionAdmin").get(adminController.seccionAdmin);
+router.route("/posteo").get(adminController.subirPost);
+router.route("/config").get(adminController.config);
 router.route("/*").get(adminController.error404);
 module.exports = router;
