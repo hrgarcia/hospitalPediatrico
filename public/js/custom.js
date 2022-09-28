58c8b161feda8ba22d4ac6d0eaaa5dd1d70756b4
JeniLopez <jelopez@escuelasproa.edu.ar>
2022-09-28 13:38:14 -0300

    botones en español y slader configurado

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,15 +1,13 @@
-
-  (function ($) {
-  
-  "use strict";
+(function ($) {
+    "use strict";
 
     // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
+    $(".navbar-nav .nav-link").click(function () {
+        $(".navbar-collapse").collapse("hide");
     });
 
     // REVIEWS CAROUSEL
-    $('.reviews-carousel').owlCarousel({
+    $(".reviews-carousel").owlCarousel({
         center: true,
         loop: true,
         nav: true,
@@ -17,34 +15,35 @@
         autoplay: true,
         autoplaySpeed: 300,
         smartSpeed: 500,
-        responsive:{
-          0:{
-            items:1,
-          },
-          768:{
-            items:2,
-            margin: 100,
-          },
-          1280:{
-            items:2,
-            margin: 100,
-          }
-        }
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+                margin: 100,
+            },
+            1280: {
+                items: 2,
+                margin: 100,
+            },
+        },
     });
 
     // Banner Carousel
-    var myCarousel = document.querySelector('#myCarousel')
+    var myCarousel = document.querySelector("#myCarousel");
     var carousel = new bootstrap.Carousel(myCarousel, {
-      interval: 1500,
-    })
+        interval: 3000,
+        touch: true,
+    });
 
     // REVIEWS NAVIGATION
-    function ReviewsNavResize(){
-      $(".navbar").scrollspy({ offset: -94 });
+    function ReviewsNavResize() {
+        $(".navbar").scrollspy({ offset: -94 });
 
-      var ReviewsOwlItem = $('.reviews-carousel .owl-item').width();
+        var ReviewsOwlItem = $(".reviews-carousel .owl-item").width();
 
-      $('.reviews-carousel .owl-nav').css({'width' : (ReviewsOwlItem) + 'px'});
+        $(".reviews-carousel .owl-nav").css({ width: ReviewsOwlItem + "px" });
     }
 
     $(window).on("resize", ReviewsNavResize);
@@ -52,17 +51,24 @@
 
     // HREF LINKS
     $('a[href*="#"]').click(function (event) {
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top - 74
-          }, 1000);
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                event.preventDefault();
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 74,
+                    },
+                    1000
+                );
+            }
         }
-      }
     });
-    
-  })(window.jQuery);
+})(window.jQuery);
